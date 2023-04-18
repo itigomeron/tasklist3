69614015efc136138ed16f1f9471b8a745a9774d
Ubuntu <ubuntu@ip-172-31-30-53.ap-northeast-1.compute.internal>
2023-04-18 09:03:17 +0000

    for fly.io

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_22_082331) do
+ActiveRecord::Schema.define(version: 2023_03_22_082824) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
